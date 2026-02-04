8e9c968c2df555c8ae0a987607ec86b58b8bab76
nargesalh <111004902+nargesalh@users.noreply.github.com>
2026-02-05 02:05:32 +0330

    Delete Final Project - Clean Code directory

--- Final Project - Clean Code/__init__.py
+++
@@ -1,2 +0,0 @@
-# attendance_clean package
-__all__ = ["core", "infra", "usecases", "presentation", "compat"]
--- Final Project - Clean Code/run_ui.py
+++
@@ -1,4 +0,0 @@
-from attendance_clean.presentation.ui_tk.app import main
-
-if __name__ == "__main__":
-    main()
